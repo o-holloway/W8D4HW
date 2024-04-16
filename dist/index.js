a18b44233cdb34d1a8e56fa6cad3c2c2e304bbe7
o-holloway <156986161+o-holloway@users.noreply.github.com>
2024-04-15 21:46:37 -0400

    final commit

--- dist/index.js
+++ dist/index.js
@@ -1,105 +1,76 @@
-import { v4 as uuidv4 } from 'uuid';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const uuid_1 = require("uuid");
 class Item {
-    private _id: string;
-    private _name: string;
-    private _price: number;
-    private _description: string;
-
-    constructor(name: string, price: number, description: string, id?: string) {
-        this._id = id ? id : uuidv4();
+    constructor(name, price, description, id) {
+        this._id = id ? id : (0, uuid_1.v4)();
         this._name = name;
         this._price = price;
         this._description = description;
     }
-
-    get id(): string {
+    get id() {
         return this._id;
     }
-
-    set id(value: string) {
+    set id(value) {
         this._id = value;
     }
-
-    get name(): string {
+    get name() {
         return this._name;
     }
-
-    set name(value: string) {
+    set name(value) {
         this._name = value;
     }
-
-    get price(): number {
+    get price() {
         return this._price;
     }
-
-    set price(value: number) {
+    set price(value) {
         this._price = value;
     }
-
-    get description(): string {
+    get description() {
         return this._description;
     }
-
-    set description(value: string) {
+    set description(value) {
         this._description = value;
     }
 }
-
 class User {
-    private _id: string;
-    private _name: string;
-    private _age: number;
-    private _cart: Item[];
-
-    constructor(name: string, age: number, cart?: Item[], id?: string) {
-        this._id = id || uuidv4();
+    constructor(name, age, cart, id) {
+        this._id = id || (0, uuid_1.v4)();
         this._name = name;
         this._age = age;
         this._cart = cart || [];
     }
-
-    get id(): string {
+    get id() {
         return this._id;
     }
-
-    set id(value: string) {
+    set id(value) {
         this._id = value;
     }
-
-    get name(): string {
+    get name() {
         return this._name;
     }
-
-    set name(value: string) {
+    set name(value) {
         this._name = value;
     }
-
-    get age(): number {
+    get age() {
         return this._age;
     }
-
-    set age(value: number) {
+    set age(value) {
         this._age = value;
     }
-
-    get cart(): Item[] {
+    get cart() {
         return this._cart;
     }
-
-    set cart(value: Item[]) {
+    set cart(value) {
         this._cart = value;
     }
-
-    addToCart(item: Item): void {
+    addToCart(item) {
         this._cart.push(item);
     }
-
-    removeFromCart(item: Item): void {
+    removeFromCart(item) {
         this._cart = this._cart.filter(cartItem => cartItem.id !== item.id);
     }
-
-    removeQuantityFromCart(item: Item, quantity: number): void {
+    removeQuantityFromCart(item, quantity) {
         let count = 0;
         this._cart = this._cart.filter(cartItem => {
             if (cartItem.id === item.id && count < quantity) {
@@ -109,61 +80,48 @@ class User {
             return true;
         });
     }
-
-    cartTotal(): number {
+    cartTotal() {
         return this._cart.reduce((total, item) => total + item.price, 0);
     }
 }
 class Shop {
-    private _items: Item[];
-
     constructor() {
         this._items = [];
         this.addItem(new Item("Fork", 10, "Stainless-steel fork"));
         this.addItem(new Item("Hatchet", 84, "Best quality hatchet"));
         this.addItem(new Item("Water Jug", 33, "Insulated water jug"));
     }
-
-    get items(): Item[] {
+    get items() {
         return this._items;
     }
-
-    addItem(item: Item): void {
+    addItem(item) {
         this._items.push(item);
     }
-
-    removeItem(item: Item): void {
+    removeItem(item) {
         this._items = this._items.filter(shopItem => shopItem.id !== item.id);
     }
 }
-
-function printCart(user: User): void {
+function printCart(user) {
     if (user.cart.length === 0) {
         console.log("The cart is empty.");
         return;
     }
-
     console.log("Cart Contents:");
     user.cart.forEach((item, index) => {
         console.log(`${index + 1}. Name: ${item.name}, Price: ${item.price}, Description: ${item.description}`);
     });
 }
-
 const shop = new Shop();
-
 const axel = new User('Axel Robles', 30);
 
-//add 2 forks, 1 hatchet and 1 jug
 axel.addToCart(shop.items[0]);
 axel.addToCart(shop.items[0]);
 axel.addToCart(shop.items[1]);
 axel.addToCart(shop.items[2]);
 printCart(axel);
 
-//remove the hatchet
 axel.removeFromCart(shop.items[1]);
 printCart(axel);
 
-//remove 1 of the 2 forks
 axel.removeQuantityFromCart(shop.items[0], 1);
 printCart(axel);
